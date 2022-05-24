efad055ad580655d997ef3dda1f51de633272d49
Anupam Bhakta <anupambhakta1@gmail.com>
2022-05-23 19:18:29 -0500

    commented out old code

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ output = np.dot(weights, inputs) + biases
 print(output)
 
 
-layer_outputs = []
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input*weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
+# layer_outputs = []
+# for neuron_weights, neuron_bias in zip(weights, biases):
+#     neuron_output = 0
+#     for n_input, weight in zip(inputs, neuron_weights):
+#         neuron_output += n_input*weight
+#     neuron_output += neuron_bias
+#     layer_outputs.append(neuron_output)
 
-print(layer_outputs)
+# print(layer_outputs)
