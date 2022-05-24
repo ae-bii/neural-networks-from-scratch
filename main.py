a69e0bb4e0f88af2615d8f84c9c2eafc0c3bc8af
Anupam Bhakta <anupambhakta1@gmail.com>
2022-05-23 20:08:28 -0500

    created dense layer class

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
+np.random.seed(0)
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-biases = [2, 3, 0.5]
-
-output = np.dot(weights, inputs) + biases
-print(output)
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
 
-# layer_outputs = []
-# for neuron_weights, neuron_bias in zip(weights, biases):
-#     neuron_output = 0
-#     for n_input, weight in zip(inputs, neuron_weights):
-#         neuron_output += n_input*weight
-#     neuron_output += neuron_bias
-#     layer_outputs.append(neuron_output)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-# print(layer_outputs)
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+        
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+# print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
